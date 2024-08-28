d5f13e5666085d7aa4a53d7658eb6ce9aa0a24f1
AlexisCro <alexis.crozier.ac@gmail.com>
2024-08-28 10:58:47 +0200

    test: integration add task

--- src/App.integration.test.tsx
+++ src/App.integration.test.tsx
@@ -2,23 +2,20 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 import { expect, test } from 'vitest';
+import React from 'react';
 
-test('renders Todo', () => {
+test('renders Todo', async () => {
   render(<App />);
   const user = userEvent.setup();
   const input = screen.getByPlaceholderText('Add a task');
   const button = screen.getByText('Add task');
 
   // Create a task
-  user.type(input, 'Buy milk');
-  user.click(button);
-  localStorage.setItem('tasks', JSON.stringify([{ value: 'Buy milk', isDone: false, urgent: false, id: 0 }]));
-  screen.logTestingPlaygroundURL();
-  console.log(localStorage)
+  await user.type(input, 'Buy milk');
+  await user.click(button);
 
   // Check if the task is rendered
   const listTodo = screen.getByTestId('task-0');
-  //console.log(screen.getByTestId('todo'))
 
-  //expect(listTodo.textContent).toBe('Buy milk');
+  expect(listTodo.textContent).toBe('Buy milk');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -249,4 +249,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
